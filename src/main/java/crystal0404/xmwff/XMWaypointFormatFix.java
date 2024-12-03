a942f406dc9b991ad4fa18c7851ac33299870c66
Crystal0404 <syh040325@163.com>
2024-12-03 12:21:47 +0800

    Initial commit

--- src/main/java/crystal0404/xmwff/XMWaypointFormatFix.java
+++ src/main/java/crystal0404/xmwff/XMWaypointFormatFix.java
@@ -1,17 +1,17 @@
 package crystal0404.xmwff;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class XMWaypointFormatFix implements ModInitializer {
+public class XMWaypointFormatFix implements ClientModInitializer {
 	public static final String MOD_ID = "xmwff";
 	public static final String MOD_NAME = "XMWaypointFormatFix";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		LOGGER.info("XMWaypointFormatFix!");
 	}
 }
